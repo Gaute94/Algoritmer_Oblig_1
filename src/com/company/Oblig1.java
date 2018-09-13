3ad275b1a29e2fd0adf8df8fd292ed17f74d822b
Gaute94 <31408331+Gaute94@users.noreply.github.com>
2018-09-13 18:20:04 +0200

    I lied, added it now

--- src/com/company/Oblig1.java
+++ src/com/company/Oblig1.java
@@ -1,4 +1,30 @@
 package com.company;
 
 public class Oblig1 {
+
+    /**
+     * Oppgave 2
+     * @param a
+     * @return Returns the amount of unique numbers in the array a.
+     */
+    public static int antallUlikeSortert(int[] a){
+        int count = 1;
+        if(a.length < 1){
+            return 0;
+        }
+        if(a.length == 1){
+            return 1;
+        }
+        for (int i = 0; i < a.length - 1; i++) {
+            int number = a[i];
+            if (number > a[i + 1]) {
+                throw new IllegalStateException("Tabellen er ikke sortert.");
+            }
+            if (number != a[i + 1]) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+
 }
